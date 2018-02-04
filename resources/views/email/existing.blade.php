4a968986ddfb79d0cc97510423f61645eeced16f
Ryan Shofner <ryun@humboldtweb.com>
2018-02-04 09:49:09 -0800

    add invite emails

---
+++ resources/views/email/existing.blade.php
@@ -0,0 +1,14 @@
+Hey {{ $user->name }},
+
+<br><br>
+
+{{ $invitation->team->owner->name }} has invited you to join their team "{{$team->name}}"!
+
+<br><br>
+
+Since you already have an account, you may accept the invitation from your
+account settings or by clicking the link below.
+
+<br><br>
+
+Thanks!
---
+++ resources/views/email/new.blade.php
@@ -0,0 +1,14 @@
+Hey,
+
+<br><br>
+
+{{ $invitation->team->owner->name }} has invited you to join their team "{{$team->name}}"!
+If you do not already have an account, you may click the following link to get started:
+
+<br><br>
+
+<a href="{{ url('register?invitation='.$invitation->token) }}">{{ url('register?invitation='.$invitation->token) }}</a>
+
+<br><br>
+
+Thanks!
